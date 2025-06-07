247778428f3e25d99f67ead3e64fc8a7d2e4981b
yuneyoungjun <0204jun@naver.com>
2025-06-08 01:24:54 +0900

    최종본

--- mobile/avoid.py
+++ mobile/avoid.py
@@ -1,58 +1,56 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import PoseWithCovarianceStamped
-import numpy as np
-import math
+from geometry_msgs.msg import Twist, PoseWithCovarianceStamped
 from sensor_msgs.msg import LaserScan
 from rclpy.qos import qos_profile_sensor_data
+import numpy as np
+import math
+import time
+
+
 class GoalNavigationNode(Node):
     def __init__(self):
         super().__init__('goal_navigation_node')
 
-        # Robot 초기 위치
+        # Robot 상태
         self.robot_x = 0.0
         self.robot_y = 0.0
         self.robot_theta = 0.0  # deg
+        self.obs_time=0.0
 
-        # 목표 위치
+        # 목표 및 웨이포인트
         self.goal_x = 0.0
         self.goal_y = 0.0
-        self.goal_theta = 0.0  # deg
-        self.waypoint=[[0.0,0.0],[2.1,0.8],[3.83, -1.02],[5.75,0.47],[0.0,0.0] ]
+        self.v=0
+        self.w=0
+        self.goal_theta =  -179.0
+        self.waypoint = [[0.0, 0.0], [2.1, 0.8], [3.83, -1.02], [5.75, 0.45], [5.74,0.0],[0.0, 0.0]]
         # self.waypoint=[[0.0,0.0],[2.0,1.0],[4.0, -1.0],[6.0,0.0],[0.0,0.0] ]
-        self.range=0.25
-        self.obs_range=0.3
-        self.scan_avg=0
+        self.waypoint=[[0.0,0.0],[2.2,0.72],[3.84, -1.0],[5.98,0.47],[4.0,-0.19],[0,0]]
+        self.waypoint=[[0.0,0.0],[2.55,0.76],[4.05, -1.1],[6.0,0.46],[0,-0.2]]
+        self.range = 0.4
 
+        # 장애물 회피
+        self.obs_range = 0.55
+        self.scan_avg = 0.0
+        self.obs = 0
+
+        # 타이머 관리
+        self.waypoint_change_time = time.time()
 
         # 제어 파라미터
         self.K_rho = 0.3
         self.K_alpha = 0.7
         self.K_beta = -0.0
-        self.mode = "1"  # 주행 방향 (전진 or 후진)
-        self.obs=0
+        self.mode = "1"
 
-        # 속도 제한
-        self.max_linear_velocity = 0.1   # m/s
-        self.max_angular_velocity = 0.5  # rad/s
+        self.max_linear_velocity = 0.1
+        self.max_angular_velocity = 0.5
 
-        # 방향 설정
         self.setDirection()
 
-        self.best_particle_sub = self.create_subscription(
-            PoseWithCovarianceStamped,
-            '/amcl_pose',
-            self.pose_callback,
-            10
-        )
-
-        self.pose_sub = self.create_subscription(
-            LaserScan,
-            "/scan",
-            self.laser_callback,
-            qos_profile_sensor_data
-        )
+        self.create_subscription(PoseWithCovarianceStamped, '/amcl_pose', self.pose_callback, 10)
+        self.create_subscription(LaserScan, "/scan", self.laser_callback, qos_profile_sensor_data)
         self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
 
@@ -68,65 +66,55 @@ class GoalNavigationNode(Node):
         pose = msg.pose.pose
         x = pose.position.x
         y = pose.position.y
-
         q = pose.orientation
         siny_cosp = 2.0 * (q.w * q.z + q.x * q.y)
         cosy_cosp = 1.0 - 2.0 * (q.y * q.y + q.z * q.z)
         theta = math.atan2(siny_cosp, cosy_cosp)
-
         self.robot_x = x
         self.robot_y = y
         self.robot_theta = self.saturationRad(theta) * 180 / np.pi
 
     def timer_callback(self):
-        v, w = self.calculateVelocity()
-        v = self.saturationVelocity(v)
-        w = self.saturationAngularVelocity(w)
-
-
-
         if self.waypoint:
-            self.goal_x=self.waypoint[0][0]
-            self.goal_y=self.waypoint[0][1]
-            self.timer
-        else:
-            v=0
-            w=0
+            self.goal_x = self.waypoint[0][0]
+            self.goal_y = self.waypoint[0][1]
 
+        self.v, self.w = self.calculateVelocity()
+        self.v = self.saturationVelocity(self.v)
+        self.w = self.saturationAngularVelocity(self.w)
 
-        # print(self.goal_x)
-        # print(self.goal_y)
         twist = Twist()
-        twist.linear.x = float(v)
-        twist.angular.z = float(w)
+        twist.linear.x = float(self.v)
+        twist.angular.z = float(self.w)
         self.cmd_vel_pub.publish(twist)
-        # self.get_logger().info(f"x={self.robot_x:.2f}, y={self.robot_y:.2f}, theta={self.robot_theta:.2f}")
-        # self.get_logger().info(f"Publishing cmd_vel: v={v:.2f}, w={w:.2f}")
 
     def calculateVelocity(self):
         dx = self.goal_x - self.robot_x
         dy = self.goal_y - self.robot_y
 
         path_theta = np.arctan2(dy, dx)
-        if self.mode == "2":
+        if self.mode == "2":# self.waypoint=[[0.0,0.0],[2.0,1.0],[4.0, -1.0],[6.0,0.0],[0.0,0.0] ]
             path_theta = self.saturationRad(path_theta + np.pi)
 
         rho = np.hypot(dx, dy)
         alpha = self.saturationRad(path_theta - np.deg2rad(self.robot_theta))
         beta = self.saturationRad(np.deg2rad(self.goal_theta) - path_theta)
 
-        v = self.K_rho * rho
-        w = self.K_alpha * alpha + self.K_beta * beta
-        # print(w)
+        self.v = self.K_rho * rho
+        self.w = self.K_alpha * alpha + self.K_beta * beta
 
-        if self.mode == "2":
-            v = -v
+        # if self.mode == "2":
+        #     v = -v
 
         if rho < self.range:
-            self.waypoint.pop(0)
-            self.obs==0
+            if self.waypoint:
+                self.waypoint.pop(0)
+                # self.obs = 0
+                self.waypoint_change_time = time.time()  # 현재 시간 저장
+                self.goal_theta = 0.0  # 초기화
+                self.setDirection()
 
-        return v, w
+        return self.v, self.w
 
     def saturationRad(self, rad):
         return (rad + np.pi) % (2 * np.pi) - np.pi
@@ -136,28 +124,65 @@ class GoalNavigationNode(Node):
 
     def saturationAngularVelocity(self, w):
         return max(-self.max_angular_velocity, min(self.max_angular_velocity, w))
-    
 
     def laser_callback(self, scan: LaserScan):
-        self.scan_avg=0
-        self.setDirection()
-        for i in range(5):
-            self.scan_avg+=scan.ranges[i]/10
-            self.scan_avg+=scan.ranges[349+i]/10
-        print(self.obs)
-        # print(self.scan_avg)
-        if self.scan_avg<self.obs_range:
-                if len(self.waypoint)%2==0:
-                    self.obs=1
-                if len(self.waypoint)%2==1:
-                    self.obs=2
+        # 웨이포인트 변경 후 3초간은 장애물 감지 무시
+        if time.time() - self.waypoint_change_time < 7.0:
+            self.obs = 0
+            return
+        if time.time()-self.obs_time<13.5:
+            return
 
+        # self.scan_avg = 0
+        count = 0
+        self.obs = 0
+        self.setDirection()
+        self.scan_avg=0
+        avg1=0
+        avg2=0
+        # self.obs=0
+        self.goal_theta=179
+        for i in range(8):
+            if scan.ranges[i]==0:
+                scan.ranges[i]=7
+            avg1+=scan.ranges[i]/16
+            avg2+=scan.ranges[349+i]/16
+            self.scan_avg=avg1+avg2
+
+        # print(self.scan_avg)
+        if 0<self.scan_avg<self.obs_range:
+                self.v=self.v/2
+                # if len(self.waypoint)%2==0:
+                #     self.obs=1
+                # if len(self.waypoint)%2==1:
+                #     self.obs=2
+                # # if len(self.waypoint)==1:
+                # #     self.obs=3
+                if(len(self.waypoint))==1:
+                    if time.time() - self.waypoint_change_time < 3.0:
+                        self.v=0
+                    if avg1>avg2:
+                        self.obs=2
+                    else:
+                        self.obs=2
+        if(len(self.waypoint))==1:
+            self.K_beta=-0.25
+        print(self.scan_avg)
         if self.obs==1:
-            self.K_beta=-0.2
-            self.goal_theta=-90
+            self.K_beta=-0.25
+            self.goal_theta=45
         elif self.obs==2:
-            self.K_beta=-0.2
-            self.goal_theta=90
+            self.K_beta=-0.25
+            self.goal_theta=45
+            self.obs_time = time.time()
+        print(self.obs)
+        if(len(self.waypoint))==0:
+            self.v=0
+            self.w=0
+        else:
+            print(self.waypoint[0])
+        # elif self.obs==3:
+        #     self.w=self.w*2
 
 def main(args=None):
     rclpy.init(args=args)
@@ -170,411 +195,6 @@ def main(args=None):
         node.destroy_node()
         rclpy.shutdown()
 
+
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import rclpy
-# from rclpy.node import Node
-# from geometry_msgs.msg import Twist
-# from geometry_msgs.msg import PoseWithCovarianceStamped
-# import numpy as np
-# import math
-# from sensor_msgs.msg import LaserScan
-# from rclpy.qos import qos_profile_sensor_data
-# class GoalNavigationNode(Node):
-#     def __init__(self):
-#         super().__init__('goal_navigation_node')
-
-#         # Robot 초기 위치
-#         self.robot_x = 0.0
-#         self.robot_y = 0.0
-#         self.robot_theta = 0.0  # deg
-
-#         # 목표 위치
-#         self.goal_x = 0.0
-#         self.goal_y = 0.0
-#         self.goal_theta = 90.0  # deg
-#         self.waypoint=[[0.0,0.0],[2.1,0.8],[3.83, -1.02],[5.75,0.47],[0.0,0.0] ]
-#         self.range=0.25
-#         self.obs_range=0.5
-#         self.scan_avg=0
-
-
-#         # 제어 파라미터
-#         self.K_rho = 0.3
-#         self.K_alpha = 0.7
-#         self.K_beta = -0.0
-#         self.mode = "1"  # 주행 방향 (전진 or 후진)
-#         self.obs=0
-
-#         # 속도 제한
-#         self.max_linear_velocity = 0.1   # m/s
-#         self.max_angular_velocity = 0.5  # rad/s
-
-#         # 방향 설정
-#         self.setDirection()
-
-#         self.best_particle_sub = self.create_subscription(
-#             PoseWithCovarianceStamped,
-#             '/amcl_pose',
-#             self.pose_callback,
-#             10
-#         )
-
-#         self.pose_sub = self.create_subscription(
-#             LaserScan,
-#             "/scan",
-#             self.laser_callback,
-#             qos_profile_sensor_data
-#         )
-#         self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
-#         self.timer = self.create_timer(0.1, self.timer_callback)
-
-#         self.get_logger().info("Goal Navigation Node Started.")
-
-#     def setDirection(self):
-#         dx = self.goal_x - self.robot_x
-#         dy = self.goal_y - self.robot_y
-#         alpha = self.saturationRad(np.arctan2(dy, dx) - np.deg2rad(self.robot_theta))
-#         self.mode = "2" if abs(alpha) > np.pi / 2 else "1"
-
-#     def pose_callback(self, msg):
-#         pose = msg.pose.pose
-#         x = pose.position.x
-#         y = pose.position.y
-
-#         q = pose.orientation
-#         siny_cosp = 2.0 * (q.w * q.z + q.x * q.y)
-#         cosy_cosp = 1.0 - 2.0 * (q.y * q.y + q.z * q.z)
-#         theta = math.atan2(siny_cosp, cosy_cosp)
-
-#         self.robot_x = x
-#         self.robot_y = y
-#         self.robot_theta = self.saturationRad(theta) * 180 / np.pi
-
-#     def timer_callback(self):
-#         v, w = self.calculateVelocity()
-#         v = self.saturationVelocity(v)
-#         w = self.saturationAngularVelocity(w)
-
-
-
-#         if self.waypoint:
-#             self.goal_x=self.waypoint[0][0]
-#             self.goal_y=self.waypoint[0][1]
-#             self.goal_theta=0
-#         else:
-#             v=0
-#             w=0
-
-
-#         print(self.goal_x)
-#         print(self.goal_y)
-#         twist = Twist()
-#         twist.linear.x = float(v)
-#         twist.angular.z = float(w)
-#         self.cmd_vel_pub.publish(twist)
-#         self.get_logger().info(f"x={self.robot_x:.2f}, y={self.robot_y:.2f}, theta={self.robot_theta:.2f}")
-#         # self.get_logger().info(f"Publishing cmd_vel: v={v:.2f}, w={w:.2f}")
-
-#     def calculateVelocity(self):
-#         dx = self.goal_x - self.robot_x
-#         dy = self.goal_y - self.robot_y
-
-#         path_theta = np.arctan2(dy, dx)
-#         if self.mode == "2":
-#             path_theta = self.saturationRad(path_theta + np.pi)
-
-#         rho = np.hypot(dx, dy)
-#         alpha = self.saturationRad(path_theta - np.deg2rad(self.robot_theta))
-#         beta = self.saturationRad(np.deg2rad(self.goal_theta) - path_theta)
-
-#         v = self.K_rho * rho
-#         w = self.K_alpha * alpha + self.K_beta * beta
-
-#         if self.mode == "2":
-#             v = -v
-
-#         if rho < self.range:
-#             self.waypoint.pop(0)
-#             self.obs==0
-
-#         return v, w
-
-#     def saturationRad(self, rad):
-#         return (rad + np.pi) % (2 * np.pi) - np.pi
-
-#     def saturationVelocity(self, v):
-#         return max(-self.max_linear_velocity, min(self.max_linear_velocity, v))
-
-#     def saturationAngularVelocity(self, w):
-#         return max(-self.max_angular_velocity, min(self.max_angular_velocity, w))
-    
-
-#     def laser_callback(self, scan: LaserScan):
-#         for i in range(10):
-#             # if len(self.waypoint)%2==0:
-#             self.scan_avg+=scan.ranges[i]/10
-#             if self.scan_avg<self.obs_range:
-#                 self.obs=1
-#         for i in range(10):
-#             # if len(self.waypoint)%2==0:
-#             self.scan_avg+=scan.ranges[349+i]/10
-#             if self.scan_avg<self.obs_range:
-#                 self.obs=2
-#             # elif self.scan_avg<self.obs_range:
-#             #     self.obs=2
-#             # else:
-#             #     if scan.ranges[i]<self.obs_range:
-#             #         self.obs=2
-#             #     elif scan.ranges[359-i]<self.obs_range:
-#             #         self.obs=2
-#         print(self.obs)
-
-#         if self.obs==1:
-#             self.K_beta=-0.15
-#             self.goal_theta=-90
-#         elif self.obs==2:
-#             self.K_beta=-0.15
-#             self.goal_theta=90
-
-# def main(args=None):
-#     rclpy.init(args=args)
-#     node = GoalNavigationNode()
-#     try:
-#         rclpy.spin(node)
-#     except KeyboardInterrupt:
-#         node.get_logger().info("Shutting down node.")
-#     finally:
-#         node.destroy_node()
-#         rclpy.shutdown()
-
-# if __name__ == '__main__':
-#     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import rclpy
-# from rclpy.node import Node
-# from geometry_msgs.msg import Twist
-# from geometry_msgs.msg import PoseWithCovarianceStamped
-# import numpy as np
-# import math
-# from sensor_msgs.msg import LaserScan
-# from rclpy.qos import qos_profile_sensor_data
-# class GoalNavigationNode(Node):
-#     def __init__(self):
-#         super().__init__('goal_navigation_node')
-
-#         # Robot 초기 위치
-#         self.robot_x = 0.0
-#         self.robot_y = 0.0
-#         self.robot_theta = 0.0  # deg
-
-#         # 목표 위치
-#         self.goal_x = 0.0
-#         self.goal_y = 0.0
-#         self.goal_theta = 0.0  # deg
-#         self.waypoint=[[0.0,0.0],[2.1,0.8],[3.83, -1.02],[5.75,0.47],[0.0,0.0] ]
-#         # self.waypoint=[[0.0,0.0],[2.0,1.0],[4.0, -1.0],[6.0,0.0],[0.0,0.0] ]
-#         self.range=0.25
-#         self.obs_range=0.3
-#         self.scan_avg=0
-
-
-#         # 제어 파라미터
-#         self.K_rho = 0.3
-#         self.K_alpha = 0.7
-#         self.K_beta = -0.0
-#         self.mode = "1"  # 주행 방향 (전진 or 후진)
-#         self.obs=0
-
-#         # 속도 제한
-#         self.max_linear_velocity = 0.1   # m/s
-#         self.max_angular_velocity = 0.5  # rad/s
-
-#         # 방향 설정
-#         self.setDirection()
-
-#         self.best_particle_sub = self.create_subscription(
-#             PoseWithCovarianceStamped,
-#             '/amcl_pose',
-#             self.pose_callback,
-#             10
-#         )
-
-#         self.pose_sub = self.create_subscription(
-#             LaserScan,
-#             "/scan",
-#             self.laser_callback,
-#             qos_profile_sensor_data
-#         )
-#         self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
-#         self.timer = self.create_timer(0.1, self.timer_callback)
-
-#         self.get_logger().info("Goal Navigation Node Started.")
-
-#     def setDirection(self):
-#         dx = self.goal_x - self.robot_x
-#         dy = self.goal_y - self.robot_y
-#         alpha = self.saturationRad(np.arctan2(dy, dx) - np.deg2rad(self.robot_theta))
-#         self.mode = "2" if abs(alpha) > np.pi / 2 else "1"
-
-#     def pose_callback(self, msg):
-#         pose = msg.pose.pose
-#         x = pose.position.x
-#         y = pose.position.y
-
-#         q = pose.orientation
-#         siny_cosp = 2.0 * (q.w * q.z + q.x * q.y)
-#         cosy_cosp = 1.0 - 2.0 * (q.y * q.y + q.z * q.z)
-#         theta = math.atan2(siny_cosp, cosy_cosp)
-
-#         self.robot_x = x
-#         self.robot_y = y
-#         self.robot_theta = self.saturationRad(theta) * 180 / np.pi
-
-#     def timer_callback(self):
-#         v, w = self.calculateVelocity()
-#         v = self.saturationVelocity(v)
-#         w = self.saturationAngularVelocity(w)
-
-
-
-#         if self.waypoint:
-#             self.goal_x=self.waypoint[0][0]
-#             self.goal_y=self.waypoint[0][1]
-#         else:
-#             v=0
-#             w=0
-
-
-#         # print(self.goal_x)
-#         # print(self.goal_y)
-#         twist = Twist()
-#         twist.linear.x = float(v)
-#         twist.angular.z = float(w)
-#         self.cmd_vel_pub.publish(twist)
-#         # self.get_logger().info(f"x={self.robot_x:.2f}, y={self.robot_y:.2f}, theta={self.robot_theta:.2f}")
-#         # self.get_logger().info(f"Publishing cmd_vel: v={v:.2f}, w={w:.2f}")
-
-#     def calculateVelocity(self):
-#         dx = self.goal_x - self.robot_x
-#         dy = self.goal_y - self.robot_y
-
-#         path_theta = np.arctan2(dy, dx)
-#         if self.mode == "2":
-#             path_theta = self.saturationRad(path_theta + np.pi)
-
-#         rho = np.hypot(dx, dy)
-#         alpha = self.saturationRad(path_theta - np.deg2rad(self.robot_theta))
-#         beta = self.saturationRad(np.deg2rad(self.goal_theta) - path_theta)
-
-#         v = self.K_rho * rho
-#         w = self.K_alpha * alpha + self.K_beta * beta
-#         # print(w)
-
-#         if self.mode == "2":
-#             v = -v
-
-#         if rho < self.range:
-#             self.waypoint.pop(0)
-#             self.obs==0
-
-#         return v, w
-
-#     def saturationRad(self, rad):
-#         return (rad + np.pi) % (2 * np.pi) - np.pi
-
-#     def saturationVelocity(self, v):
-#         return max(-self.max_linear_velocity, min(self.max_linear_velocity, v))
-
-#     def saturationAngularVelocity(self, w):
-#         return max(-self.max_angular_velocity, min(self.max_angular_velocity, w))
-    
-
-#     def laser_callback(self, scan: LaserScan):
-#         self.scan_avg=0
-#         for i in range(5):
-#             self.scan_avg+=scan.ranges[i]/10
-#             self.scan_avg+=scan.ranges[349+i]/10
-#         print(self.obs)
-#         # print(self.scan_avg)
-#         if self.scan_avg<self.obs_range:
-#                 if len(self.waypoint)%2==0:
-#                     self.obs=1
-#                 if len(self.waypoint)%2==1:
-#                     self.obs=2
-
-#         if self.obs==1:
-#             self.K_beta=-0.2
-#             self.goal_theta=-90
-#         elif self.obs==2:
-#             self.K_beta=-0.2
-#             self.goal_theta=90
-
-# def main(args=None):
-#     rclpy.init(args=args)
-#     node = GoalNavigationNode()
-#     try:
-#         rclpy.spin(node)
-#     except KeyboardInterrupt:
-#         node.get_logger().info("Shutting down node.")
-#     finally:
-#         node.destroy_node()
-#         rclpy.shutdown()
-
-# if __name__ == '__main__':
-#     main()
-
-
-
-
-
-
-
-
-
-
--- mobile/waypoint_pub.py
+++ mobile/waypoint_pub.py
@@ -6,21 +6,17 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseArray, Pose
 from tf_transformations import euler_from_quaternion, quaternion_from_euler
 from rclpy.qos import qos_profile_sensor_data
-from visualization_msgs.msg import Marker, MarkerArray
 
 class LivePlotter(Node):
     def __init__(self):
         super().__init__('live_plotter')
-        self.robot_x = 0.0
-        self.robot_y = 0.0
+        self.robot_x = 0.5
+        self.robot_y = 0.5
         self.robot_theta = 0.0
-        self.waypoints = [[0.0,0.0]]
-        self.current_waypoint_idx = 0
+        self.waypoints = []
+        self.current_waypoint_idx = None
         self.laser_ranges = []
         self.laser_angles = []
-        self.first=0
-        self.initial_x=0
-        self.initial_x=0
 
         # ROS 2 구독자
         self.create_subscription(PoseWithCovarianceStamped, '/amcl_pose', self.pose_callback, 10)
@@ -34,7 +30,7 @@ class LivePlotter(Node):
         )
 
         # ROS 2 퍼블리셔 (클릭으로 추가된 웨이포인트 지속 발행)
-        self.waypoint_pub = self.create_publisher(MarkerArray, '/waypoints', 10)
+        self.waypoint_pub = self.create_publisher(PoseArray, '/waypoints', 10)
 
         # Matplotlib 시각화
         plt.ion()
@@ -45,7 +41,6 @@ class LivePlotter(Node):
         self.timer = self.create_timer(0.1, self.timer_callback)
 
     def pose_callback(self, msg):
-        self.first+=1
         self.robot_x = msg.pose.pose.position.x
         self.robot_y = msg.pose.pose.position.y
         orientation_q = msg.pose.pose.orientation
@@ -58,7 +53,6 @@ class LivePlotter(Node):
         angle_min = msg.angle_min
         angle_increment = msg.angle_increment
         self.laser_angles = angle_min + np.arange(len(self.laser_ranges)) * angle_increment
-        print(len(msg.ranges))
 
     def onclick(self, event):
         """ ✅ 마우스 클릭 시 좌표를 웨이포인트로 추가 """
@@ -75,32 +69,25 @@ class LivePlotter(Node):
         self.publish_waypoints()
 
     def publish_waypoints(self):
-        marker_array = MarkerArray()
+        msg = PoseArray()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = "map"
 
-        for i, (x, y) in enumerate(self.waypoints):
-            marker = Marker()
-            marker.header.frame_id = "map"
-            marker.header.stamp = self.get_clock().now().to_msg()
-            marker.ns = "waypoints"
-            marker.id = i
-            marker.type = Marker.SPHERE
-            marker.action = Marker.ADD
-            marker.pose.position.x = x
-            marker.pose.position.y = y
-            marker.pose.position.z = 0.2
-            marker.pose.orientation.w = 1.0
-            marker.scale.x = 0.3
-            marker.scale.y = 0.3
-            marker.scale.z = 0.3
-            marker.color.r = 1.0
-            marker.color.g = 0.0
-            marker.color.b = 0.0
-            marker.color.a = 1.0
-            marker.lifetime.sec = 0  # 0이면 무한
+        for wp in self.waypoints:
+            pose = Pose()
+            pose.position.x = wp[1]*np.cos(self.robot_theta)
+            pose.position.y = wp[0]*np.cos(self.robot_theta)
+            pose.position.x = wp[1]
+            pose.position.y = wp[0]
+            pose.position.z = 0.0
+            qx, qy, qz, qw = quaternion_from_euler(0, 0, 0)
+            pose.orientation.x = qx
+            pose.orientation.y = qy
+            pose.orientation.z = qz
+            pose.orientation.w = qw
+            msg.poses.append(pose)
 
-            marker_array.markers.append(marker)
-
-        self.waypoint_pub.publish(marker_array)
+        self.waypoint_pub.publish(msg)
 
     def update_plot(self):
         self.ax.clear()
